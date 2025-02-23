9e4b00788773950c458532cdb4c1a040bc6c165e
Marco Fernández Callejón <marco.fernandezcallejon@hotmail.com>
2025-02-23 21:45:15 +0100

    Add files via upload

---
+++ #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/solution_00.py
@@ -0,0 +1,14 @@
+#https://www.python.org
+
+'''
+esto es otro comentario
+de varias lineas
+'''
+my_variable = "Mi variable"
+
+str = "hola"
+float = 8.9
+int = 4
+bool = True
+
+print ("Hola, python!")
